b48b717423cca41b70cbccfc934ccdec068a1d53
glae83 <glae83@gmail.com>
2020-06-23 11:25:39 +0400

    Lesson3

---
+++ src/test1/lesson3.java
@@ -0,0 +1,15 @@
+package test1;
+
+import java.util.*;
+public class lesson3 {
+    public static void main(String [] args) {
+String [] words = {
+        "Car", "Sun" , "Road", "Car", "Table", "Around", "Value", "Watch" , "Road", "Mouse"
+};
+        Map<String, Integer> numCount = new HashMap<>();
+        for (String num : words) {
+            numCount.put(num, numCount.getOrDefault(num,0)+1);
+        }
+        System.out.println(numCount);
+    }
+}
